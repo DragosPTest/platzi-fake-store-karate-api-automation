0b2fb7ef23bca3537728d8d39a15a0e628582bf4
DragosPTest <draggospopescu@gmail.com>
2025-09-29 22:50:19 +0300

    remove @test tag, remove testRunner logic, keep the scenario

--- src/test/java/runners/testRunner.java
+++ src/test/java/runners/testRunner.java
@@ -1,18 +1,2 @@
-package runners;
-
-import com.intuit.karate.Results;
-import com.intuit.karate.Runner;
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
-
-import java.io.File;
-
-public class testRunner {
-    @Test
-    void testParallel() {
-        Results results = Runner.path("classpath:features/auth/auth.feature", "classpath:features/users/users.feature").tags("@regression").parallel(5);
-        assertEquals(0, results.getFailCount(), results.getErrorMessages());
-        System.out.println("Running features from: " + new File("src/test/resources/features").getAbsolutePath());
-
-    }
+package runners;public class testRunner {
 }
